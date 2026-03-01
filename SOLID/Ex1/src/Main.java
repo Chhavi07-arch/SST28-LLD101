4950617681792e74af01bdf8575cbd7d5dc512f6
Chhavi07-arch <chhavi.24bcs10201@sst.scaler.com>
2026-03-01 23:49:12 +0530

    Added solutions till Ex6

--- SOLID/Ex1/src/Main.java
+++ SOLID/Ex1/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("=== Student Onboarding ===");
+
         FakeDb db = new FakeDb();
         OnboardingService svc = new OnboardingService(db);
 
